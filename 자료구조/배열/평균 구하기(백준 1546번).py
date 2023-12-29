1b33852ccc6d2d32f2bb8d4e870544511103b31b
홍성민 <landsky1234@naver.com>
2023-12-29 11:06:35 +0900

    Delete 자료구조/배열 directory

--- 자료구조/배열/평균 구하기(백준 1546번).py
+++
@@ -1,8 +0,0 @@
-subject_num = int(input())
-
-subject_score = list(map(int, input().split()))
-max_score = max(subject_score)
-
-for i in range(subject_num):
-    subject_score[i] = (subject_score[i] / (max_score * 100)) * 10000
-print(sum(subject_score) / len(subject_score))
