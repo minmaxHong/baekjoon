62ff2f5cbc21f587d3c4c00f820cb191d7b14674
홍성민 <landsky1234@naver.com>
2024-01-04 14:04:38 +0900

    Update 스택으로 수열 만들기(백준 1874).py

--- 자료구조/스택으로 수열 만들기(백준 1874).py
+++ 자료구조/스택으로 수열 만들기(백준 1874).py
@@ -1,42 +1,36 @@
-# 주의사항
-# 1. push하는 순서는 오름차순(1,2,3,4 ~)
-# 2. push 연산은 "+"
-# 3. pop 연산은 "-"로 출력
-# 4. 불가능할때는 "NO" 출력
-
 import sys
 input = sys.stdin.readline
 
-num = int(input())
+stack_length = int(input())
 stack = []
+sign = []
 n = 0
-output = []
-
 flag = True
 
-for i in range(num):
+for i in range(stack_length):
     stack_num = int(input())
     
     if stack_num >= n:
         while True:
             n += 1
-            stack.append(n)
-            output.append("+")
             if stack_num == n:
+                stack.append(n)
+                sign.append("+")
                 stack.pop()
-                output.append("-")
+                sign.append("-")
                 break
-            
+            stack.append(n)
+            sign.append("+")
     else:
         top = stack.pop()
-        
         if top < stack_num:
             print("NO")
             flag = False
             break
-        else:
-            output.append("-")
 
-if flag:
-    for i in range(len(output)):
-        print(output[i])
+        sign.append("-")
+
+if flag == True:
+    for i in range(len(sign)):
+        print(sign[i])
+    
